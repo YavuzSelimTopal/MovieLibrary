45b4d900797104bf0d5b360d8eb29b911871bc3e
YavuzSelimTopal <yavuzselimtopal80@gmail.com>
2025-05-22 02:41:54 +0300

    SecondComit

--- MovieLibrary/ContentView.swift
+++ MovieLibrary/ContentView.swift
@@ -8,17 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = UserViewModel() // ViewModel bağlandı
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                // Kullanıcılar döngüye alınıyor
+                ForEach(viewModel.users, id: \.id) { user in
+                    VStack(alignment: .leading) {
+                        Text(user.name) // Kullanıcının adı
+                            .font(.headline)
+
+                        Text("@\(user.username)") // Kullanıcının kullanıcı adı
+                            .foregroundColor(.gray)
+
+                        Text(user.email) // Kullanıcının e-posta adresi
+                            .font(.subheadline)
+
+                        Text(user.address.city) // Kullanıcının yaşadığı şehir
+                            .font(.caption)
+                            .foregroundColor(.secondary)
+                    }
+                    .padding(.vertical, 5)
+                }            }
+            .navigationTitle("Kullanıcılar")
+            .task {
+                await viewModel.fetchUsers() // Sayfa açılınca veriyi çek
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
--- MovieLibrary/UserViewModel.swift
+++ MovieLibrary/UserViewModel.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+@MainActor
+class UserViewModel: ObservableObject {
+    @Published var users: [UserElement] = []
+
+    private let apiClient = APIClient()
+
+    func fetchUsers() async {
+        do {
+            let result = try await apiClient.fetch(
+                urlString: "https://jsonplaceholder.typicode.com/users",
+                responseType: [UserElement].self
+            )
+            
+            self.users = result
+        } catch {
+            print("Hata: \(error.localizedDescription)")
+        }
+    }
+}
